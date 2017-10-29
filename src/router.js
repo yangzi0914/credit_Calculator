89b32ec4ffa5e4884877a739f01c885102d57034
liyang <liyang2@gomefinance.com>
2017-10-30 02:28:05 +0800

    feat: 爱滑冰首页

---
+++ src/router.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Router, Route, Switch } from 'dva/router';
+import IndexPage from './routes/IndexPage';
+import home from './routes/Home';
+
+function RouterConfig({ history }) {
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route path="/" exact component={home} />
+        <Route path="/IndexPage" exact component={IndexPage} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default RouterConfig;
---
+++ src/routes/Home/Nav.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TweenOne from 'rc-tween-one';
+import { Menu } from 'antd';
+import logo from '../../assets/logo@2x.png';
+
+const Item = Menu.Item;
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      phoneOpen: false,
+    };
+  }
+
+  phoneClick = () => {
+    this.setState({
+      phoneOpen: !this.state.phoneOpen,
+    });
+  }
+
+  render() {
+    const props = { ...this.props };
+    const isMode = props.isMode;
+    delete props.isMode;
+    const navData = { menu1: '首页', menu2: '资讯', menu3: '公司介绍'};
+    const navChildren = Object.keys(navData)
+      .map((key, i) => (<Item key={i}><a href="/IndexPage">{navData[key]}</a></Item>));
+    return (<TweenOne
+      component="header"
+      animation={{ opacity: 0, type: 'from' }}
+      {...props}
+    >
+      <TweenOne
+        className={`${this.props.className}-logo`}
+        animation={{ x: -30, type: 'from', ease: 'easeOutQuad' }}
+        id={`${this.props.id}-logo`}
+      >
+        <img width="100%" src={logo}/>
+      </TweenOne>
+      {isMode ? (<div
+        className={`${this.props.className}-phone-nav${this.state.phoneOpen ? ' open' : ''}`}
+        id={`${this.props.id}-menu`}
+      >
+        <div
+          className={`${this.props.className}-phone-nav-bar`}
+          onClick={() => {
+            this.phoneClick();
+          }}
+        >
+          <em />
+          <em />
+          <em />
+        </div>
+        <div
+          className={`${this.props.className}-phone-nav-text`}
+        >
+          <Menu
+            defaultSelectedKeys={['0']}
+            mode="inline"
+            theme="dark"
+          >
+            {navChildren}
+          </Menu>
+        </div>
+      </div>) : (<TweenOne
+        className={`${this.props.className}-nav`}
+        animation={{ x: 30, type: 'from', ease: 'easeOutQuad' }}
+      >
+        <Menu
+          mode="horizontal" defaultSelectedKeys={['0']}
+          id={`${this.props.id}-menu`}
+        >
+          {navChildren}
+        </Menu>
+      </TweenOne>)}
+    </TweenOne>);
+  }
+}
+
+Header.propTypes = {
+  className: PropTypes.string,
+  dataSource: PropTypes.object,
+  id: PropTypes.string,
+};
+
+Header.defaultProps = {
+  className: 'header0',
+};
+
+export default Header;
