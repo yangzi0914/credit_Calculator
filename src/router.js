7d93d73ef69b48a358854d42923e10727da62cc1
liyang <liyang2@gomefinance.com>
2017-12-29 19:02:46 +0800

    feat: 服务内容

--- src/router.js
+++ src/router.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import Home from './routes/Home';
-import Information from './routes/Information/information';
-import InforDetail from './routes/Information/inforDetail';
+// import Information from './routes/Information/information';
+// import InforDetail from './routes/Information/inforDetail';
 import Company from './routes/Company/company';
+import Service from './routes/Service/service';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/information" exact component={Information} />
-        <Route path="/inforDetail/:id" exact component={InforDetail} />
+        {/* <Route path="/information" exact component={Information} /> */}
+        {/* <Route path="/inforDetail/:id" exact component={InforDetail} /> */}
         <Route path="/company" exact component={Company} />
+        <Route path="/service" exact component={Service} />
       </Switch>
     </Router>
   );
